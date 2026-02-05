d9985c1fa2c336d0bc10040e2f310a49ca4f1e89
cyberdeeb <abrahamdeeb@gmail.com>
2026-02-05 10:20:04 -0800

    chore: update package.json for eslint and typescript-eslint integration
    - Added eslint and typescript-eslint dependencies to package.json.
    - Introduced a lint script for code quality checks.
    - Updated globals version for compatibility.
    
    test: modify auth.test.ts imports
    
    - Removed unused import 'test' from vitest in auth.test.ts.
    
    feat: add eslint configuration file
    
    - Created eslint.config.js to define linting rules and configurations.
    - Configured eslint to use recommended settings for JavaScript and TypeScript.

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it, test } from "vitest";
+import { describe, expect, it } from "vitest";
 import { getAPIKey } from "../api/auth";
 import { IncomingHttpHeaders } from "http";
 
